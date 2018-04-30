788e00db49c80aee66da8bef33b0c2c90cab986e
teamwhosoff <38849741+teamwhosoff@users.noreply.github.com>
2018-04-30 18:22:02 +0530

    Update firebase.config.ts

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,9 +1,9 @@
 // AF2 Settings
 export const FirebaseConfig = {
-  apiKey: "AIzaSyA9t3RPOQndRBcyB0xXmceaVOxTh0eDwKQ",
-  authDomain: "ee-lms.firebaseapp.com",
-  databaseURL: "https://ee-lms.firebaseio.com",
-  projectId: "ee-lms",
-  storageBucket: "ee-lms.appspot.com",
-  messagingSenderId: "750036322505"
+    apiKey: "AIzaSyCQAooVAj_pXVg4NBYo7iTka5jCfas8DM4",
+    authDomain: "whosoff.firebaseapp.com",
+    databaseURL: "https://whosoff.firebaseio.com",
+    projectId: "whosoff",
+    storageBucket: "whosoff.appspot.com",
+    messagingSenderId: "753802534970"
 };
